7caebed635b72dc444b91fc6da7fbf9015178fb9
git <barache.nadia@gmail.com>
2013-09-24 18:28:53 +0200

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,38 @@
+
+# ------  Note ------------#
+   # params : Contient des infos sur chaque requete
+
+
+
+
+
 class UsersController < ApplicationController
   def new
   	#le titre personnalisé 
   	# pour la page de nouvel utilisateur
+    @user =User.new
   	@titre ="Inscription"
   end
+ 
   def show 
      # trouver les utilisateurs avec leur id
   	 @user =User.find(params[:id])
      @titre =@user.nom
   end
+  
   def index
+  end
+
+  def create
+    @user =User.new(params[:user])
+    if @user.save
+      flash[:success] = "Bienvenue !" 
+      redirect_to @user
+      # traite un succés d'enregistrement
+    else
+      @titre="Inscription"
+      render 'new'
+    end
   end
 
 end
